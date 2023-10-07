43fb550e311b844ce0be507c0005fda6de9d4466
sohelranaweb <sohel.rana.web2@gmail.com>
2023-10-07 13:34:35 +0600

    project setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>This is home page</h1>
+    </div>
+  );
+};
+
+export default Home;
